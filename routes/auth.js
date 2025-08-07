3ea08f186ba8217fbaf2616bad7cd091097db108
Ankunda Beth <ankundabeth07@gmail.com>
2025-08-07 13:08:54 +0300

    adding public files

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user'); // We'll create this next
-const { saveOTP } = require("../utils/otpStore");
+
 
 
 
--- server.js
+++ server.js
@@ -24,3 +24,7 @@ mongoose.connect(process.env.MONGO_URI,)
 app.listen(PORT, () => {
     console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
