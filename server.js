d861df915d1437db63f7a6f5504bc73953520ff7
Ankunda Beth <ankundabeth07@gmail.com>
2025-08-08 13:14:35 +0300

    fixing server

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
+
 // Middleware
 app.use(express.json());
 app.use(cors());
@@ -16,15 +17,21 @@ const authRoutes = require('./routes/auth');
 app.use('/api', authRoutes);
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI,)
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('✅ Connected to MongoDB'))
   .catch(err => console.error('❌ MongoDB connection error:', err));
+
+// Routes
+app.get('/', (req, res) => 
+    res.sendFile(path.join(__dirname, 'public', 'home.html'))
+);
+
+app.get('/user/:id', (req, res) => {
+    const userId = req.params.id;
+    res.json({ message: `User ID is ${userId}` });
+});
 
 // Start server
 app.listen(PORT, () => {
     console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'login.html'));
 });
