e6760667f66794fc2b0c45e21b0ccb3463a2fdde
Ankunda Beth <ankundabeth07@gmail.com>
2025-08-20 16:54:29 +0300

    reinstalled bycript js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const bcrypt = require('bcryptjs');
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
