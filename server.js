8735649cf9cd857234c77eb8ea4d9c2d4d2b35cd
Ankunda Beth <ankundabeth07@gmail.com>
2025-08-20 17:17:47 +0300

    right frontend link

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: ["https://yamba-platform-wrgflu4ny-ankundabeth07-8205s-projects.vercel.app/"], // allow only your frontend
+  origin: ["https://yamba-platform.vercel.app/"], // allow only your frontend
   credentials: true,
 }));
 
